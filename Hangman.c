43c8d989ee3bfb3eed9ea4c34a88937cbb93d5ef
Sara Mahfouz <saramahfouz29@gmail.com>
2026-03-08 13:15:51 +0300

    Update game loop logic and add repeated letter check

--- Hangman.c
+++ Hangman.c
@@ -10,16 +10,16 @@ int main() {
 	printf("You have a right of 7 mistakes total.\n");
 	char kelimeler[][15]={"apple","watermelon","grapes","mango","orange","lemon","cherry","strawberry"};
 	int kelimeSaysi=8;
-	int yanlis=1;
+	
 	int hataS=0;
 	char tahmin;
 	char gorunen[20];
-	int dogruS;
-	int i,j,k,s;
+	
+	int i,k,s;
 	int tamam;
 	
 	int oyunBitti=0;
-	int tamamlandi;
+	
 	
 	srand(time(NULL));
 	int RastgeleIndix=rand() %kelimeSaysi;
@@ -33,26 +33,44 @@ int main() {
 	}
 	gorunen[uzunluk]='\0';
 	
+	char girilenHarfler[30];
+	int girilenSayisi = 0;
+	
 	while(!oyunBitti)
 	{
-		printf("Fruit Name: ");
+		printf("\nFruit Name: ");
 		for(i=0;i<uzunluk;i++)
 		{
 			printf("%c",gorunen[i]);
 		}
-		printf("Guess the letters\n");
+		printf("\nGuess the letters\n");
 		scanf(" %c", &tahmin);
+		int zatenGirildi = 0;
+		for(k = 0; k < girilenSayisi; k++) {
+			if(girilenHarfler[k] == tahmin) {
+				zatenGirildi = 1;
+				break;
+			}
+		}
+		
+		if(zatenGirildi) {
+			printf("\nYou already guessed '%c'. Try a different letter.\n", tahmin);
+			continue; 
+		}
+		girilenHarfler[girilenSayisi] = tahmin;
+		girilenSayisi++;
+		int yanlis=1;
 		for(k=0;k<uzunluk;k++){
 			if(kelime[k]==tahmin)
 			{
 				gorunen[k]=tahmin;
-				dogruS++;
+				
 				yanlis=0;
 			}
 		}
 		if(yanlis){
 			hataS++;
-			printf("You entered a wrong letter, You made %d mistake",hataS);
+			printf("\nYou entered a wrong letter. You made %d mistake(s).\n", hataS);
 		}
 		tamam=1;
 		for(s=0;s<uzunluk;s++)
@@ -63,11 +81,11 @@ int main() {
 			}
 		}
 		if(tamam){
-			printf("Congratiolations");
+			printf("Congratulations");
 			oyunBitti=1;
 		}
 		else if(hataS>=7){
-			printf("You Lost, The Word was %s",kelime);
+			printf("\nYou Lost, The Word was %s\n",kelime);
 			oyunBitti=1;
 		}
 	}
